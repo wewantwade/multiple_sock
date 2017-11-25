719331614cbde72c412e6d797d40e2565de06398
wewantwade <1012382501@qq.com>
2017-11-25 14:28:11 +0800

    socket_pro

---
+++ cli1.py
@@ -0,0 +1,145 @@
+
+import os
+import pickle
+import socket
+import threading
+from tkinter import *
+from tkinter.ttk import *
+import struct#二进制转换模块
+
+
+#获取要备份的所有文件的信息
+def get_file_info(path):
+    if not path or not os.path.exists(path):
+        return None
+    files=os.walk(path)#os模块中的walk方法获取路径中的所有文件
+    print('文件:',files)
+    #定义文件信息列表和文件路径的列表
+    infos=[]
+    file_paths=[]
+    #p是指当前文件的文件路径，ds是指当前文件夹内的所有文件夹，fs是指当前文件夹内的所有文件
+    for p,ds,fs in files:
+        # print('p:',p)
+        # print('ds:', ds)
+        # print('fs:',fs)
+        for f in fs:
+            file_name=os.path.join(p,f)#获取文件名并添加到当前路径下
+            file_size=os.stat(file_name).st_size#获取当前文件的大小
+            file_paths.append(file_name)
+            print('file_paths:',file_paths)
+            file_name=file_name[len(path)+1:]
+            print('file_name:',file_name)
+            infos.append((file_size,file_name))
+            print('infos:',infos)
+    return infos,file_paths
+
+#向服务器端发送文件信息
+def send_files_infos(my_sock,infos):
+    fmt_str='Q'
+    #对所有的文件信息进行二进制编码
+    infos_bytes=pickle.dumps(infos)
+    #获取二进制编码后的文件信息的长度
+    infos_bytes_len=len(infos_bytes)
+    #将长度也进行二进制编码
+    infos_len_pack=struct.pack(fmt_str,infos_bytes_len)
+    my_sock.sendall(infos_len_pack)
+    my_sock.sendall(infos_bytes)
+
+
+#向服务器发送所有文件
+def send_files(my_sock,file_path):
+    f=open(file_path,'rb')
+    try:
+        while True:
+            data=f.read(1024)
+            if data:
+                my_sock.sendall(data)
+            else:
+                break
+    finally:
+        f.close()
+
+#接收反馈信息
+def get_bak_info(my_sock,size=7):
+    info=my_sock.recv(size)
+    print(info.decode('utf-8'))
+
+#启动服务器
+def start(host,port,src):
+    if not os.path.exists(src):
+        print('备份的目标不存在')
+        return
+    s=socket.socket()
+    s.connect((host,port))
+    path=src
+    #获取每一个文件信息和路径
+    file_infos,file_paths=get_file_info(path)
+    # #发送文件信息
+    # send_files_infos(s,file_infos)
+    # for fp in file_paths:
+    #     #发送文件列表中的每一个文件
+    #     send_files(s,fp)
+    #     print(fp)
+    #     #获取备份结果是否成功
+    #     get_bak_info(s)
+    # s.close()
+
+#定义一个类继承frame
+class MyFrame(Frame):
+    #定义初始化方法
+    def __init__(self,root):
+        super().__init__(root)
+        self.root=root
+        self.grid()#网格布局
+        #定义要连接的服务器IP和端口
+        self.remote_ip='127.0.0.1'
+        self.remote_port=10888
+        #用户要连接的ip输入框,端口输入框，备份文件的输入框
+        self.remote_ip_var=StringVar()
+        self.remote_ports_var=IntVar()
+        self.bak_src_var=StringVar()
+        self.init_components()#定义一个界面的初始化方法
+
+    #界面初始化方法
+    def init_components(self):
+        #窗口名称
+        project_name=Label(self,text="远程备份客户机")
+        project_name.grid(columnspan=2)
+        #IP标签
+        serv_ip_label=Label(self,text="服务地址:")
+        serv_ip_label.grid(row=1)
+        #IP输入框
+        self.serv_ip=Entry(self,textvariable=self.remote_ip_var)
+        self.remote_ip_var.set(self.remote_ip)
+        self.serv_ip.grid(row=1,column=1)
+        #端口标签
+        serv_port_label=Label(self,text="服务端口:")
+        serv_port_label.grid(row=2)
+        #端口输入框
+        self.serv_port=Entry(self,textvariable=self.remote_ports_var)
+        self.remote_ports_var.set(self.remote_port)
+        self.serv_port.grid(row=2,column=1)
+        #用户的备份文件目录
+        src_lable=Label(self,text="备份的目标:")
+        src_lable.grid(row=3)
+
+        self.bak_src=Entry(self,textvariable=self.bak_src_var)
+        self.bak_src.grid(row=3,column=1)
+        #启动服务和关闭服务的端口
+        self.start_serv_btn=Button(self,text="开始备份",command=self.start_send)
+        self.start_serv_btn.grid(row=4)
+        self.start_exit_btn=Button(self,text="退出服务",command=self.root.destroy)
+        self.start_exit_btn.grid(row=4,column=1)
+
+    def start_send(self):
+        print(self.remote_ip_var.get(),self.remote_ports_var.get())
+        print('start.......')
+        start(self.remote_ip_var.get(),int(self.remote_ports_var.get()),self.bak_src_var.get())
+
+if __name__=='__main__':
+    root=Tk()
+    root.title('备份客户机')
+    # root.geometry('200x100')
+    root.resizable(True,True)#设置大小不可变
+    app=MyFrame(root)
+    app.mainloop()
---
+++ ser1.py
@@ -0,0 +1,159 @@
+import os
+import pickle
+import socket
+import threading
+from tkinter import *
+from tkinter.ttk import *
+import struct#二进制转换模块
+
+#定义一个默认备份目录
+BAK_PATH='/home/funingchen/Desktop/hh/'
+
+#启动服务器
+def start(host,port):
+    if not os.path.exists(BAK_PATH):
+        os.mkdir(BAK_PATH)
+    st=socket.socket()
+    st.bind((host,port))
+    st.listen(1)
+    client,addr=st.accept()
+    #接收发送方的文件信息，包括各个文件的大小和路径
+    files_lst=get_files_info(client)#定义一个方法get_files获取客户端发送的文件信息列表
+    for size,filepath in files_lst:#遍历列表中的文件大小和路径
+        res=recv_file(client,size,filepath)#定义recv_file函数备份文件（大小，路径）
+        send_echo(client,res)#备份完成一个文件，就发送一次成功备份的信息
+    #备份完成关闭套接字和客户端链接
+    client.close()
+    st.close()
+
+#接收文件信息，返回文件数据
+def recv_unit_data(clnt,infos_len):
+    data=b''
+    #如果文件信息长度小于1024，一次性进行接收，如果大于1024，则分多次进行接收信息
+    if 0<infos_len<=1024:
+        data+=clnt.recv(infos_len)
+    else:
+        while True:
+            if infos_len>1024:
+                data+=clnt.recv(1024)
+                infos_len-=1024
+            else:
+                data+=clnt.recv(infos_len)
+                break
+    return data
+
+
+#获取文件信息
+def get_files_info(clnt):
+    #定义一个格式串，代表向服务器端发送文件信息的大小Q，长整数
+    fmt_str='Q'
+    #计算出fmt_str长整形数的大小
+    headsize=struct.calcsize(fmt_str)
+    #根据计算结果接受此信息
+    data=clnt.recv(headsize)
+    #将接收到的信息解开，获取文件信息大小
+    infos_len=struct.unpack(fmt_str,data)[0]
+    #根据文件信息大小接收文件信息
+    data=recv_unit_data(clnt,infos_len)
+    return pickle.loads(data)#客户端发送的文件信息是经过pickle二进制编码的，此时需要解开获取到文件的大小和路径列表
+
+#建立文件路径函数（当客户端发送的文件路径在默认备份路径下不存在时，要建立这个文件路径）
+def mk_path(filepath):
+    paths=filepath.split(os.path.sep)[:-1]#将文件名进行分割并去掉文件名
+    p=BAK_PATH
+    for path in paths:
+        # 遍历客户端传来的路径，将这些路径逐一添加到默认路径上面
+        p=os.path.join(p,path)
+        if not os.path.exists(p):
+            os.mkdir(p)
+
+#备份文件的主方法
+def recv_file(clnt,infos_len,filepath):
+    #首先根据客户端发送的文件路径对路径进行建立
+    mk_path(filepath)
+    #建立服务器上的文件路径，包括文件名
+    filepath=os.path.join(BAK_PATH,filepath)
+    #打开建立好的文件，写入数据
+    f=open(filepath,'wb+')
+    try:
+        if 0<infos_len<1024:
+            data=clnt.recv(infos_len)
+            f.write(data)
+        else:
+            while True:
+                if infos_len>1024:
+                    data=clnt.recv(1024)
+                    f.write(data)
+                    infos_len-=1024
+                else:
+                    data=clnt.recv(infos_len)
+                    f.write(data)
+                    break
+    except:
+        print('error')
+    else:
+        return True
+    finally:
+        f.close()
+
+#向客户端发送是否备份成功的消息
+def send_echo(clnt,res):
+    if res:
+        clnt.sendall(b'success')
+    else:
+        clnt.sendall(b'failure')
+#定义一个类继承frame
+class MyFrame(Frame):
+    #定义初始化方法
+    def __init__(self,root):
+        super().__init__(root)
+        self.root=root
+        self.grid()#网格布局
+        self.local_ip=['127.0.0.1']
+        self.serv_ports=[10888,20888,30888]#服务器端口
+        self.init_components()#定义一个界面的初始化方法
+
+    #界面初始化方法
+    def init_components(self):
+        #窗口名称
+        project_name=Label(self,text="远程备份服务器")
+        project_name.grid(columnspan=2)
+        #IP标签
+        serv_ip_label=Label(self,text="服务地址:")
+        serv_ip_label.grid(row=1)
+        #IP输入框
+        self.serv_ip=Combobox(self,values=self.get_ipaddr())
+        self.serv_ip.set(self.local_ip)
+        self.serv_ip.grid(row=1,column=1)
+        #端口标签
+        serv_port_label=Label(self,text="服务端口")
+        serv_port_label.grid(row=2)
+        #端口输入框
+        self.serv_port=Combobox(self,values=self.serv_ports)
+        self.serv_port.set(self.serv_ports[0])
+        self.serv_port.grid(row=2,column=1)
+        #启动服务和关闭服务的端口
+        self.start_serv_btn=Button(self,text="启动服务",command=self.start_serv)
+        self.start_serv_btn.grid(row=3)
+        self.start_exit_btn=Button(self,text="退出服务",command=self.root.destroy)
+        self.start_exit_btn.grid(row=3,column=1)
+
+    #获取主机IP地址方法
+    def get_ipaddr(self):
+        host_name=socket.gethostname()
+        print('hostname是:',host_name)
+        info=socket.gethostbyname_ex(host_name)#到到主机的相关信息
+        info=info[2]
+        info.append(self.local_ip)
+        return info
+    def start_serv(self):
+        print(self.serv_ip.get(),self.serv_port.get())
+        start(self.serv_ip.get(),int(self.serv_port.get()))
+
+if __name__=='__main__':
+    root=Tk()
+    root.title('备份服务器')
+    # root.geometry('200x100')
+    root.resizable(True,True)#设置大小不可变
+    app=MyFrame(root)
+    app.mainloop()
